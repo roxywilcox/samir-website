6ad2e7e98d3e1846c8fcc3ab907940a199ac4b8c
roxywilcox <roxygwilcox@gmail.com>
2021-04-20 20:06:10 -0500

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,18 +5,33 @@ import { NavDropdown, Nav, Navbar } from "react-bootstrap";
 function NavBar() {
   return (
     <div className="nav-bar-container">
-      <div className="wrapper">
-        <img src={samirPic} className="khuller-image" alt="kuller" />
-        <div className="title-wrapper">
-          <div className="name">Samir Khuller</div>
-          <div className="position1">
-            Peter and Adrienne Barris Chair of Computer Science
+      <div className="title-card-container">
+        <div className="wrapper">
+          <img src={samirPic} className="khuller-image" alt="kuller" />
+          <div className="title-wrapper">
+            <div className="name">Samir Khuller</div>
+            <div className="position1">
+              Peter and Adrienne Barris Chair of Computer Science
+            </div>
+            <div className="position2">Professor of Computer Science</div>
+            <div className="school">Northwestern University</div>
           </div>
-          <div className="position2">Professor of Computer Science</div>
-          <div className="school">Northwestern University</div>
         </div>
       </div>
-      <Navbar expand="lg" variant="dark" className="top-nav justify-content-center">
+      <div className="nav">
+        <a href="/home">Home</a>
+        <a href="/teaching">Teaching</a>
+        <NavDropdown title="Students" id="basic-nav-dropdown">
+          <NavDropdown.Item className="dropdown-item" href="/alumni">Alumni</NavDropdown.Item>
+          <NavDropdown.Item className="dropdown-item" href="/phd">PhD Students</NavDropdown.Item>
+          <NavDropdown.Item className="dropdown-item" href="/undergrad">Undergraduates</NavDropdown.Item>
+        </NavDropdown>
+        <a href="/profEngagements">Professional Engagements</a>
+        <a href="/publications">Publications</a>
+        <a href="/talks">Talks</a>
+        <a href="/bio">Bio</a>
+      </div>
+      {/* <Navbar expand="lg" variant="dark" className="top-nav justify-content-center">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" id="nav-container">
@@ -33,7 +48,7 @@ function NavBar() {
             <Nav.Link href="/bio">Bio</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
+      </Navbar> */}
     </div>
   );
 }
