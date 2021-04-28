401521342eb7dd4f3318f41198417632dcd8401f
roxywilcox <roxygwilcox@gmail.com>
2021-04-28 12:43:18 -0500

    added to home page, small edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,49 @@
 import "./Home.css";
 
-
 function Home() {
   return (
     <div>
-        <div className="home-title">
-            Research Interests
+      <div className="home-title">Research Interests</div>
+      <div className="blurb-container">
+        <div className="blurb">
+          My work is in the areas of Algorithm Design, specifically on
+          graph algorithms, combinatorics, scheduling and approximation
+          algorithms. Specifically, my research is motivated by algorithmic
+          questions arising in the context of data storage, communication,
+          clustering and load balancing. I have published widely on these areas,
+          and have advised a number of graduate and undergraduate students who
+          worked in these topics.
         </div>
+      </div>
+      <div className="home-title">Current Work / About Me</div>
+      <div className="blurb-container">
+        <div className="blurb">
+          From 2003-2008, I was the Assoc. Chair for Graduate Studies in
+          Computer Science at the University of Maryland, and then from
+          2012-2017, I was the inaugural Elizabeth Iribe Chair for CS at
+          University of Maryland. In 2019, I joined Northwestern Computer
+          Science, a newly created independent Computer Science department,
+          where I am currently the Peter and Adrienne Barris Chair for Computer
+          Science and a Professor in the McCormick School of Engineering and
+          Applied Science.
+        </div>
+      </div>
+      <div className="home-title">Contact Me</div>
+      <div className="contact-container">
         <div className="contact">
-            short blurb here!
-            <br/>
-            <br/>
+          2233 Tech Drive
+          <br />
+          Mudd Room 3017
+          <br />
+          Evanston, IL 60208-3109
+          <br />
+          <br />
+          847-491-2748
+          <br />
+          <br />
+          samir.khuller@northwestern.edu
         </div>
-        <div className="home-title">
-            Featured Work
-        </div>
-        <div className="contact">
-            short blurb here!
-            <br/>
-            <br/>
-        </div>
-        <div className="home-title">
-            Contact Me
-        </div>
-        <div className="contact">
-            2233 Tech Drive
-            <br/>
-            Mudd Room 3017
-            <br/>
-            Evanston, IL 60208-3109
-            <br/>
-            <br/>
-            
-            847-491-2748
-            <br/>
-            <br/>
-            samir.khuller@northwestern.edu
-        </div>
+      </div>
     </div>
   );
 }
