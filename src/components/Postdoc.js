7ab7112ec5f2037956b40c17c6d06e176fb87f86
roxywilcox <roxygwilcox@gmail.com>
2021-05-24 20:19:54 -0500

    added postdocs

--- src/components/Postdoc.js
+++ src/components/Postdoc.js
@@ -1,24 +1,27 @@
-import "./Talks.css";
-import { talksData } from "./data/talks_data";
+import "./Postdoc.css";
+import { postdocData } from "./data/postdoc_data";
 
-function Postdoc() {
-    // placeholder code
+function Phd() {
   return (
-    <div className="talks-container">
-        <div className="talks-title">Talks</div>
-        <br />
-        {talksData.map((talk) => (
-          <div className="talk-wrapper">
-            <a className="talk-link" href={talk.slides} download>
-              <div className="talk-title">{talk.title}</div>
-            </a>
-
-            <div className="talk-conference">{talk.conference}</div>
+    <div className="postdoc-overall-container">
+      <div className="postdoc-title">PhD Students</div>
+      <div className="postdoc-container">
+        {postdocData.map((postdoc) => (
+          <div className="postdoc-item-container">
             <br />
+            <br />
+            <a href={postdoc.website}>
+              <img src={postdoc.img} className="postdoc-profile-img" alt="prof-pic" />
+            </a>
+            <br />
+            <br/>
+            <br/>
+            <div className="postdoc-name">{postdoc.name}</div>
           </div>
         ))}
+      </div>
     </div>
   );
 }
 
-export default Postdoc;
+export default Phd;
---
+++ src/components/data/postdoc_data.js
@@ -0,0 +1,29 @@
+import Quanquan from "../../assets/quanquanliu.jpg";
+import Sami from "../../assets/sami.jpeg";
+import Vaggos from "../../assets/vaggos.png";
+import Hedyeh from "../../assets/hedyeh.jpeg";
+
+
+export const postdocData =
+[
+  {
+    "name": "Quanquan Liu",
+    "img": Quanquan,
+    "website": "https://quanquancliu.com/",
+  },
+  {
+    "name": "Sami Davies",
+    "img": Sami,
+    "website": "http://samidavies.com/",
+  },
+  {
+    "name": "Vaggos Chatziafratis",
+    "img": Vaggos,
+    "website": "https://cs.stanford.edu/~vaggos/",
+  },
+  {
+    "name": "Hedyeh Beyhaghi",
+    "img": Hedyeh,
+    "website": "https://home.ttic.edu/~hedyeh/",
+  },
+];
